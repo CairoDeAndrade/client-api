11f584d010d9574ea2dbcd1885d092331f207adb
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2023-02-18 19:30:39 -0300

    Final event IT test passed (updateShouldReturnNotFoundWhenIdDoesNotExist)

--- tdd-challenge/src/main/java/com/devsuperior/bds02/services/EventService.java
+++ tdd-challenge/src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.bds02.services;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFound;
 
 @Service
 public class EventService {
@@ -18,12 +20,18 @@ public class EventService {
 	@Transactional
 	public EventDTO updateById(EventDTO eventDto, Long id) {
 		
-		Event entity = repositoy.getReferenceById(id);
-		entity.setName(eventDto.getName());
-		entity.setDate(eventDto.getDate());
-		entity.setUrl(eventDto.getUrl());
-		
-		entity = repositoy.save(entity);
-		return new EventDTO(entity);
+		try {
+			Event entity = repositoy.getReferenceById(id);
+			entity.setName(eventDto.getName());
+			entity.setDate(eventDto.getDate());
+			entity.setUrl(eventDto.getUrl());
+			entity = repositoy.save(entity);
+			
+			return new EventDTO(entity.getId(), entity.getName(), entity.getDate(), entity.getUrl(), eventDto.getCityId());
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFound("Id not found: " + id);
+		}
+
 	}
 }
